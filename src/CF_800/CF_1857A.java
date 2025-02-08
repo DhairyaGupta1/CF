09ca35b2ba78c8e70b7186bcb0103340485311a2
SMITA GUPTA <dhairya.gupta2022@vitstudent.ac.in>
2025-02-08 22:36:31 +0530

    Added Solution to 1857A, 1831A and 1845A

---
+++ src/CF_800/CF_1857A.java
@@ -0,0 +1,76 @@
+package CF_800;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+import static java.lang.Integer.parseInt;
+
+public class CF_1857A {
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader()
+        {
+            br = new BufferedReader(
+                    new InputStreamReader(System.in));
+        }
+
+        String next()
+        {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() { return parseInt(next()); }
+
+        long nextLong() { return Long.parseLong(next()); }
+
+        double nextDouble()
+        {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine()
+        {
+            String str = "";
+            try {
+                if(st == null)  st = new StringTokenizer(br.readLine());
+                if(st.hasMoreTokens()){
+                    str = st.nextToken("\n");
+                }
+                else{
+                    str = br.readLine();
+                }
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static void main(String[] args){
+        FastReader input = new FastReader();
+        int t = input.nextInt();
+
+        while(t-- > 0){
+            int n = input.nextInt(), cntOdd = input.nextInt();
+            for(int i = 1; i < n; i++)  if(input.nextInt() % 2 == 1)    cntOdd++;
+            int cntEven = n - cntOdd;
+            if(cntOdd % 2 == 1) System.out.println("NO");
+//            else if(cntOdd == 0 &&  cntEven < 2)    System.out.println("NO");
+            else System.out.println("YES");
+        }
+    }
+
+}
