6c8f44e6388d54f975a2f2da354439b4bdf878ff
SMITA GUPTA <dhairya.gupta2022@vitstudent.ac.in>
2025-02-09 23:06:53 +0530

    Added CF_1837A

---
+++ src/CF_800/CF_1837A.java
@@ -0,0 +1,80 @@
+package CF_800;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+import static java.lang.Integer.parseInt;
+
+public class CF_1837A {
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader()
+        {
+            br = new BufferedReader(
+                    new InputStreamReader(System.in));
+        }
+
+        String next()
+        {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() { return parseInt(next()); }
+
+        long nextLong() { return Long.parseLong(next()); }
+
+        double nextDouble()
+        {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine()
+        {
+            String str = "";
+            try {
+                if(st == null)  st = new StringTokenizer(br.readLine());
+                if(st.hasMoreTokens()){
+                    str = st.nextToken("\n");
+                }
+                else{
+                    str = br.readLine();
+                }
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static void main(String[] args){
+        FastReader input = new FastReader();
+
+        int t = input.nextInt();
+
+        while(t-- > 0){
+            int n = input.nextInt(), x = input.nextInt();
+            if(n % x != 0){
+                System.out.println(1);
+                System.out.println(n);
+            }
+            else{
+                System.out.println(2);
+                System.out.println((n - 1) + " " + 1);
+            }
+        }
+    }
+
+}
