4f459da2af8efb627222cdb36b7f033b3b482406
SMITA GUPTA <dhairya.gupta2022@vitstudent.ac.in>
2025-02-14 19:08:51 +0530

    Added Solution to 1869A, 1878C, 1875A and 1855B

---
+++ src/CF_900/CF_1855B.java
@@ -0,0 +1,83 @@
+package CF_900;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+import static java.lang.Integer.parseInt;
+
+public class CF_1855B {
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader()
+        {
+            br = new BufferedReader(
+                    new InputStreamReader(System.in));
+        }
+
+        String next()
+        {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() { return parseInt(next()); }
+
+        long nextLong() { return Long.parseLong(next()); }
+
+        double nextDouble()
+        {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine()
+        {
+            String str = "";
+            try {
+                if(st == null)  st = new StringTokenizer(br.readLine());
+                if(st.hasMoreTokens()){
+                    str = st.nextToken("\n");
+                }
+                else{
+                    str = br.readLine();
+                }
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static void main(String[] args){
+        FastReader input = new FastReader();
+
+        int t = input.nextInt();
+        while(t-- > 0){
+            long n = input.nextLong();
+
+            if(n % 2 == 1)  System.out.println(1);
+            else{
+                long cnt = 0;
+                for(long i = 1; i <= n; i++){
+                    if(n % i == 0)  cnt++;
+                    else break;
+                }
+
+                System.out.println(cnt);
+            }
+
+        }
+    }
+
+}
