f9daf80df0b214b4e4d8b263592255a743a6b56d
SMITA GUPTA <dhairya.gupta2022@vitstudent.ac.in>
2025-03-03 22:02:04 +0530

    Added solution to CF_1675B

--- src/ProblemSet/CF_2067A.java
+++ src/ProblemSet/CF_2067A.java
@@ -68,7 +68,6 @@ public class CF_2067A {
             if(diff == 1)   System.out.println("Yes");
             else if((diff + 1) / 9.0 % 1 == 0)  System.out.println("Yes");
             else System.out.println("No");
-
         }
     }
 }
