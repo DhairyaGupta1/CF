2610ec7024eb6efb56493aba3e9a496f39915533
SMITA GUPTA <dhairya.gupta2022@vitstudent.ac.in>
2025-03-09 21:40:46 +0530

    Answer to Luntik and Subsequences

---
+++ src/CF_900/CF_1582B.java
@@ -0,0 +1,76 @@
+package CF_900;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class CF_1582B {
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader()
+        {
+            br = new BufferedReader(
+                    new InputStreamReader(System.in));
+        }
+
+        String next()
+        {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() { return Integer.parseInt(next()); }
+
+        long nextLong() { return Long.parseLong(next()); }
+
+        double nextDouble()
+        {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine()
+        {
+            String str = "";
+            try {
+                if(st == null)  st = new StringTokenizer(br.readLine());
+                if(st.hasMoreTokens()){
+                    str = st.nextToken("\n");
+                }
+                else{
+                    str = br.readLine();
+                }
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static void main(String[] args){
+        FastReader input = new FastReader();
+        int t = input.nextInt();
+
+        while(t-- > 0){
+            int n = input.nextInt();
+            long[] arr = new long[n], nums = new long[2];
+
+            for(int i = 0; i < n; i++) {
+                arr[i] = input.nextLong();
+                if(arr[i] == 0) nums[0]++;
+                else if(arr[i] == 1)    nums[1]++;
+            }
+            System.out.println(nums[1] * (long)Math.pow(2, nums[0]));
+        }
+    }
+}
