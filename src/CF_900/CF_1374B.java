6f7ff5a73735c98058ea835e250a777c1fbd868f
SMITA GUPTA <dhairya.gupta2022@vitstudent.ac.in>
2025-04-11 22:26:20 +0530

    Added solution to CF_1374B

---
+++ src/CF_900/CF_1374B.java
@@ -0,0 +1,85 @@
+package CF_900;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class CF_1374B {
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader()
+        {
+            br = new BufferedReader(
+                    new InputStreamReader(System.in));
+        }
+
+        String next()
+        {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() { return Integer.parseInt(next()); }
+
+        long nextLong() { return Long.parseLong(next()); }
+
+        double nextDouble()
+        {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine()
+        {
+            String str = "";
+            try {
+                if(st == null)  st = new StringTokenizer(br.readLine());
+                if(st.hasMoreTokens()){
+                    str = st.nextToken("\n");
+                }
+                else{
+                    str = br.readLine();
+                }
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static void main(String[] args){
+        FastReader input = new FastReader();
+        int t = input.nextInt();
+
+        while(t-- > 0){
+            long n = input.nextLong();
+
+            int num2 = 0, num3 = 0;
+
+            while(n % 2 == 0){
+                n /= 2;
+                num2++;
+            }
+
+            while(n % 3 == 0){
+                n /= 3;
+                num3++;
+            }
+
+            if(n != 1 || num2 > num3)  System.out.println(-1);
+            else{
+                System.out.println(num3 - num2 + num3);
+            }
+        }
+    }
+}
