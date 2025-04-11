8536846f14c4060ca0375d0f58ad02148aec2005
SMITA GUPTA <dhairya.gupta2022@vitstudent.ac.in>
2025-04-12 01:07:28 +0530

    01 Game

---
+++ src/CF_900/CF_1373B.java
@@ -0,0 +1,83 @@
+package CF_900;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class CF_1373B {
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader()
+        {
+            br = new BufferedReader(
+                    new InputStreamReader(System.in));
+        }
+
+        String next()
+        {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() { return Integer.parseInt(next()); }
+
+        long nextLong() { return Long.parseLong(next()); }
+
+        double nextDouble()
+        {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine()
+        {
+            String str = "";
+            try {
+                if(st == null)  st = new StringTokenizer(br.readLine());
+                if(st.hasMoreTokens()){
+                    str = st.nextToken("\n");
+                }
+                else{
+                    str = br.readLine();
+                }
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static void main(String[] args){
+        FastReader input = new FastReader();
+        int t = input.nextInt();
+
+        while(t-- > 0){
+            String s = input.nextLine();
+            int n = s.length();
+
+            boolean alice = false;
+
+            Stack<Character> stk = new Stack<>();
+            for(char c : s.toCharArray()){
+                if(!stk.isEmpty() && stk.peek() != c){
+                    alice = !alice;
+                    stk.pop();
+                }
+                else    stk.push(c);
+            }
+
+            System.out.println(alice ? "DA" : "NET");
+        }
+    }
+}
