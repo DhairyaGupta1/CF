4e70668e9d75556717c35b33cdcb862f3901a922
SMITA GUPTA <dhairya.gupta2022@vitstudent.ac.in>
2025-05-11 02:50:19 +0530

    Added Solution to CF_190D and 1659A

---
+++ src/CF_1000/CF_1659A.java
@@ -0,0 +1,78 @@
+package CF_1000;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class CF_1659A {
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader()
+        {
+            br = new BufferedReader(
+                    new InputStreamReader(System.in));
+        }
+
+        String next()
+        {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() { return Integer.parseInt(next()); }
+
+        long nextLong() { return Long.parseLong(next()); }
+
+        double nextDouble()
+        {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine()
+        {
+            String str = "";
+            try {
+                if(st == null)  st = new StringTokenizer(br.readLine());
+                if(st.hasMoreTokens()){
+                    str = st.nextToken("\n");
+                }
+                else{
+                    str = br.readLine();
+                }
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    private static void solve(FastReader input){
+        int n = input.nextInt(), r = input.nextInt(), b = input.nextInt();
+        int p = r/(b + 1), q = r % (b + 1);
+
+        for(int i = 0; i < q; i++)  System.out.print("R".repeat(p + 1) + "B");
+        for(int i = 0; i < b + 1 - q; i++)  System.out.print("R".repeat(p) + (i == b - q ? "" : "B"));
+        System.out.println();
+    }
+
+    public static void main(String[] args){
+        FastReader input = new FastReader();
+        int t = input.nextInt();
+
+        while(t-- > 0){
+            solve(input);
+        }
+    }
+
+}
---
+++ src/CF_1000/CF_1690D.java
@@ -0,0 +1,90 @@
+package CF_1000;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class CF_1690D {
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader()
+        {
+            br = new BufferedReader(
+                    new InputStreamReader(System.in));
+        }
+
+        String next()
+        {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() { return Integer.parseInt(next()); }
+
+        long nextLong() { return Long.parseLong(next()); }
+
+        double nextDouble()
+        {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine()
+        {
+            String str = "";
+            try {
+                if(st == null)  st = new StringTokenizer(br.readLine());
+                if(st.hasMoreTokens()){
+                    str = st.nextToken("\n");
+                }
+                else{
+                    str = br.readLine();
+                }
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    private static void solve(FastReader input){
+        int n = input.nextInt(), k = input.nextInt();
+        int b = 0, last = 0, min = n;
+
+        String s = input.nextLine();
+        for(int i = 0; i < k; i++){
+            if(s.charAt(i) == 'B')  b++;
+        }
+
+        min = Math.min(min, k - b);
+        for(int i = k; i < n; i++){
+            if(s.charAt(last) == 'B')  b--;
+            if(s.charAt(i) == 'B')  b++;
+
+            min = Math.min(min, k - b);
+            last++;
+        }
+
+        System.out.println(min);
+    }
+
+    public static void main(String[] args){
+        FastReader input = new FastReader();
+        int t = input.nextInt();
+
+        while(t-- > 0){
+            solve(input);
+        }
+    }
+
+}
